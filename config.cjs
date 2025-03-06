d889237b9d364ab5a6870196108ded912e1eef4e
Teddytech1 <kibetke4488@gmail.com>
2025-03-06 11:39:37 +0300

    Update config.cjs

--- config.cjs
+++ config.cjs
@@ -24,9 +24,9 @@ const config = {
   REJECT_CALL: process.env.REJECT_CALL !== undefined ? process.env.REJECT_CALL === 'true' : false, 
   NOT_ALLOW: process.env.NOT_ALLOW !== undefined ? process.env.NOT_ALLOW === 'true' : true,
   MODE: process.env.MODE || "public",
-  OWNER_NAME: process.env.OWNER_NAME || "ℓσя∂ ʝσєℓ",
-  OWNER_NUMBER: process.env.OWNER_NUMBER || "254717108466",
-  SUDO_NUMBER: process.env.SUDO_NUMBER || "254717108466",
+  OWNER_NAME: process.env.OWNER_NAME || "Kibet",
+  OWNER_NUMBER: process.env.OWNER_NUMBER || "254799963583",
+  SUDO_NUMBER: process.env.SUDO_NUMBER || "254799963583",
   GEMINI_KEY: process.env.GEMINI_KEY || "AIzaSyCUPaxfIdZawsKZKqCqJcC-GWiQPCXKTDc",
   WELCOME: process.env.WELCOME !== undefined ? process.env.WELCOME === 'true' : false, 
 };
